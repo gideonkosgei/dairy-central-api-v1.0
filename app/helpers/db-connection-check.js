f6ecc1130f55c4567cb891f18b4dfe0981151cf0
Kipkosgei <G.Kipkosgei@cgiar.org>
2020-05-14 12:16:10 +0300

    remove exposure of db-configs

--- app/helpers/db-connection-check.js
+++ app/helpers/db-connection-check.js
@@ -4,6 +4,7 @@ const mysql = require('mysql');
 module.exports = async (params) => new Promise(
   (resolve, reject) => {
     const connection = mysql.createConnection(params);
+    console.log(params);
     connection.connect(error => {
       if (error) {
         reject(error);       
