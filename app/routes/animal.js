6fb067a7f823f063616048ae358214fe994733c6
Kipkosgei <G.Kipkosgei@cgiar.org>
2020-05-15 13:46:39 +0300

    modified animal table aliases

--- app/routes/animal.js
+++ app/routes/animal.js
@@ -9,17 +9,17 @@ router.get('/api/v1.0/animal', async (req, res) => {
     const conn = await connection(dbConfig).catch(e => {return e;}); 
     
     let sql = `select 
-    ID ANIMAL_ID,
-    animal.tag_id TAG, 
-    animal.name NAME,    
-    animal.birthdate BIRTH_DATE,
-    animal.reg_date REG_DATE,
-    (select label from core_master_list list where list.list_type_id = 13 and list.value = animal.sire_type ) as SIRE_TYPE,
-    animal.sire_tag_id SIRE_TAG, 
-    animal.dam_tag_id DAM_TAG,
-    (select label from core_master_list list where list.list_type_id = 3 and list.value = animal.sex ) as SEX,  
-    (select label from core_master_list list where list.list_type_id = 8 and list.value = animal.main_breed ) as BREED, 
-    (select label from core_master_list list where list.list_type_id = 14 and list.value = animal.breed_composition ) as BREED_COMPOSITION     
+    id,
+    animal.tag_id tag, 
+    animal.name name,    
+    animal.birthdate birt_date,
+    animal.reg_date reg_date,
+    (select label from core_master_list list where list.list_type_id = 13 and list.value = animal.sire_type ) as sire_type,
+    animal.sire_tag_id sire_tag, 
+    animal.dam_tag_id dam_tag,
+    (select label from core_master_list list where list.list_type_id = 3 and list.value = animal.sex ) as sex,  
+    (select label from core_master_list list where list.list_type_id = 8 and list.value = animal.main_breed ) as breed, 
+    (select label from core_master_list list where list.list_type_id = 14 and list.value = animal.breed_composition ) as breed_composition     
     from core_animal  animal order by id limit 100`; 
 
     const payload = await query(conn, sql).catch(e=>{return e;});    
