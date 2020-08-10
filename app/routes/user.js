ff3a076195a386ce72f47ce8bb2aecda0b8f1858
Kipkosgei <G.Kipkosgei@cgiar.org>
2020-08-10 15:17:21 +0300

    bugs removal. api length

--- app/routes/user.js
+++ app/routes/user.js
@@ -12,40 +12,23 @@ router.get('/api/v1.0/user/auth', async (req, res) => {
   let password = req.query.password; 
     
   const sql = `CALL sp_authenticate('${username}','${password}')`;    
-  const payload = await query(conn, sql).catch(e=>{return e;}); 
- 
-  const payLoadLength  = JSON.stringify(payload[0].length); 
-  const payload_code = payload.code
-  if(payload_code == 'ER_PARSE_ERROR'){
-        res.status(400).json({status:400, payload })
-  } else if (payLoadLength<1) {
-        res.status(200).json({status:204, payload });
-  } else {
-        res.status(200).json({status:200, payload });
-  }   
+  await query(conn, sql).then(response => {res.status(200).json({payload:response})}).catch(e=>{res.status(400).json({status:400, message:e })});
+   
 });
 
-router.get('/api/v1.0/user/:id', async (req, res) => {
-      const conn = await connection(dbConfig).catch(e => {return e;}); 
+router.get('/api/v1.0/user/:id', async (req, res) => {      
+      const conn = await connection(dbConfig).catch(e => {return e;});     
       const id = req.params.id;
-      const sql = `select * from v_user_profiles where id = ${id}`;    
-      const payload = await query(conn, sql).catch(e=>{return e;});     
-      const payload_code = payload.code 
-      const payLoadLength = (payload_code == 'ER_PARSE_ERROR')? 0: JSON.stringify(payload[0].length);
-      if(payload_code == 'ER_PARSE_ERROR'){
-            res.status(400).json({status:400, payload })
-      } else if (payLoadLength<1) {
-            res.status(200).json({status:204, payload });
-      } else {
-            res.status(200).json({status:200, payload });
-      } 
-    });
-  
-router.get('/api/v1.0/user', async (req, res) => {
-    const conn = await connection(dbConfig).catch(e => {return e;}); 
-    const sql = "select * from v_user_profiles";    
-    const payload = await query(conn, sql).catch(e=>{return e;});   
-    res.send(payload);
+      const sql = `CALL sp_view_user_profiles(${id})`;
+      await query(conn, sql).then(response => {res.status(200).json({payload:response})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
+  });
+
+
+router.get('/api/v1.0/user/:id', async (req, res) => {      
+      const conn = await connection(dbConfig).catch(e => {return e;});     
+      const id = req.params.id;
+      const sql = `CALL sp_view_user_profiles(${id})`;
+      await query(conn, sql).then(response => {res.status(200).json({payload:response})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
   });
 
   module.exports = router
