9dbc25b5440204bc738ec6fc3821a03d3ba32c80
Kipkosgei <G.Kipkosgei@cgiar.org>
2020-09-02 18:45:07 +0300

    added endpoint to handle milk batch actions i.e validate,discard,progress

--- app/routes/batches.js
+++ app/routes/batches.js
@@ -41,11 +41,9 @@ router.get('/api/v1.0/batches/milking/validation/:uuid', async (req, res) => {
 router.post('/api/v1.0/batches/milking/action', async (req, res) => { 
   const {action,uuid,user} = req.body; 
   const conn = await connection(dbConfig).catch(e => {return e;});     
-  const sql = `CALL sp_batch_process_milking_action(${JSON.stringify(uuid)},${action},${user})`; 
-  console.log(sql);        
+  const sql = `CALL sp_batch_process_milking_action(${JSON.stringify(uuid)},${action},${user})`;         
   await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
 });
-
 
 // view batch records based on organization and step. Filters -> org_id, step
 router.get('/api/v1.0/batches/milking/:org/:step/:user', async (req, res) => {
