35feb25fd3aa3f6ac23a48c01b5db8f5fc9ab275
Kipkosgei <G.Kipkosgei@cgiar.org>
2020-09-30 12:19:43 +0300

    added endpoint to updated service providers details

--- app/routes/partners.js
+++ app/routes/partners.js
@@ -19,13 +19,14 @@ router.post('/api/v1.0/partners/service-provider', async (req, res) => {
   await query(conn, sql).then(e => {res.status(200).json({status:200, message:"success"})}).catch(e=>{res.status(400).json({status:400, message:e })});      
 });
 
-// system parameterization - update  local settings
-router.put('/api/v1.0/partners/service-provider/:param_id', async (req, res) => {  
-  const param_id = req.params.param_id;      
+router.put('/api/v1.0/partners/service-provider/:id', async (req, res) => {        
   const conn = await connection(dbConfig).catch(e => {return e;}); 
-  const {description,is_active,key,value,updated_by,farm_id} = req.body;                     
-  const sql = `CALL sp_update_parameter_local_settings(${param_id},${JSON.stringify(key)},${JSON.stringify(value)},${is_active} ,${JSON.stringify(description)},${farm_id},${updated_by})`; 
+  const{id}  = req.params;
+  const {name, acronym, service_provider_type, country, postal_address ,postal_code, city ,phone ,email ,description ,services_offered ,contact_person ,contact_person_mobile_number ,org_id, updated_by} = req.body; 
+  const sql = `CALL sp_service_provider_update(${id},${JSON.stringify(name)}, ${JSON.stringify(acronym)}, ${service_provider_type}, ${country}, ${JSON.stringify(postal_address)} ,${JSON.stringify(postal_code)}, ${JSON.stringify(city)} ,${JSON.stringify(phone)} ,${JSON.stringify(email)} ,${JSON.stringify(description)} ,${JSON.stringify(services_offered)} ,${JSON.stringify(contact_person)} ,${JSON.stringify(contact_person_mobile_number)} ,${org_id}, ${updated_by})`;  
   await query(conn, sql).then(e => {res.status(200).json({status:200, message:"success"})}).catch(e=>{res.status(400).json({status:400, message:e })});      
 });
+
+
  
 module.exports = router
