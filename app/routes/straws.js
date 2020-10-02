ce4a14f62d1cd4ce0da7348af3b3b53b85b9469d
Kipkosgei <G.Kipkosgei@cgiar.org>
2020-10-02 17:07:10 +0300

    modification of ai straw endpoint

--- app/routes/straws.js
+++ app/routes/straws.js
@@ -22,8 +22,8 @@ router.post('/api/v1.0/straws', async (req, res) => {
 router.put('/api/v1.0/straws/:id', async (req, res) => {        
   const conn = await connection(dbConfig).catch(e => {return e;}); 
   const{id}  = req.params;
-  const {straw_id,barcode,bull_tag_id,bull_name,breed,breed_composition,semen_source,farm_name,batch_number,ejaculation_number,production_date,specification,additional_info,updated_by} = req.body; 
-  const sql = `CALL sp_ai_straw_update(${id},${JSON.stringify(straw_id)}, ${JSON.stringify(barcode)}, ${JSON.stringify(bull_tag_id)}, ${JSON.stringify(bull_name)} ,${breed}, ${breed_composition}, ${JSON.stringify(semen_source)}, ${JSON.stringify(farm_name)},${JSON.stringify(batch_number)} ,${JSON.stringify(ejaculation_number)} ,${JSON.stringify(production_date)}, ${JSON.stringify(specification)} ,${JSON.stringify(additional_info)},${updated_by} )`;   
+  const {straw_id,barcode,bull_tag_id,bull_name,breed,breed_composition,semen_source,farm_name,batch_number,ejaculation_number,production_date,specification,additional_info,updated_by,is_active} = req.body; 
+  const sql = `CALL sp_ai_straw_update(${id},${JSON.stringify(straw_id)}, ${JSON.stringify(barcode)}, ${JSON.stringify(bull_tag_id)}, ${JSON.stringify(bull_name)} ,${breed}, ${breed_composition}, ${JSON.stringify(semen_source)}, ${JSON.stringify(farm_name)},${JSON.stringify(batch_number)} ,${JSON.stringify(ejaculation_number)} ,${JSON.stringify(production_date)}, ${JSON.stringify(specification)} ,${JSON.stringify(additional_info)},${updated_by},${is_active} )`;   
   await query(conn, sql).then(e => {res.status(200).json({status:200, message:"success"})}).catch(e=>{res.status(400).json({status:400, message:e })});      
 });
 
