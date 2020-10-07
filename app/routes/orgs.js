35f0314b5b89f81d696c1ccf0ee73bec2bf2d963
Kipkosgei <G.Kipkosgei@cgiar.org>
2020-10-07 18:00:17 +0300

    new endpoint: switch org

--- app/routes/orgs.js
+++ app/routes/orgs.js
@@ -13,10 +13,10 @@ router.get('/api/v1.0/orgs', async (req, res) => {
 router.get('/api/v1.0/orgs/access/:id', async (req, res) => {   
   const user_id = req.params.id;   
   const conn = await connection(dbConfig).catch(e => {return e;});     
-  const sql = `CALL sp_org_access_view(${user_id})`;   
-  console.log(sql);      
+  const sql = `CALL sp_org_access_view(${user_id})`;        
   await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
 });
+
 router.put('/api/v1.0/orgs/access/:id', async (req, res) => {   
   const user = req.params.id;   
   const {orgs,created_by} = req.body;  
@@ -34,7 +34,14 @@ router.put('/api/v1.0/orgs/access/:id', async (req, res) => {
   }
   const conn = await connection(dbConfig).catch(e => {return e;});     
   const sql = `CALL sp_org_access_create(${JSON.stringify(org_access)},${user},${created_by})`;       
-  await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
+  await query(conn, sql).then(e => {res.status(200).json({status:200, message:"success"})}).catch(e=>{res.status(400).json({status:400, message:e })});      
+});
+
+router.put('/api/v1.0/orgs/switch/access', async (req, res) => {   
+  const conn = await connection(dbConfig).catch(e => {return e;});  
+  const {org,user} = req.body;    
+  const sql = `CALL sp_org_switch(${org},${user})`;        
+  await query(conn, sql).then(e => {res.status(200).json({status:200, message:"success"})}).catch(e=>{res.status(400).json({status:400, message:e })});       
 });
 
 module.exports = router
