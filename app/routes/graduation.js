e2192582d9f0b7ad053cd991fece2209d07ae0e0
Gideon kosgei <mr.gkosgei@gmail.com>
2020-10-19 14:41:36 +0300

    new endpoint: graduation list

--- app/routes/graduation.js
+++ app/routes/graduation.js
@@ -11,6 +11,12 @@ router.get('/api/v1.0/graduation/settings/:org_id', async (req, res) => {
   await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
 });
 
+router.get('/api/v1.0/graduation/list/:org_id/:status_id', async (req, res) => {
+  const{org_id,status_id}  = req.params;    
+  const conn = await connection(dbConfig).catch(e => {return e;});     
+  const sql = `CALL sp_graduation_list_view(${org_id},${status_id})`;         
+  await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
+});
 
 
 module.exports = router
