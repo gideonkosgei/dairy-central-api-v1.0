57c1b4ed5a1e8f64d3cb70ea11ce366db5ec19a6
Gideon kosgei <mr.gkosgei@gmail.com>
2020-10-21 14:36:40 +0300

    new endpoint ; view bp record

--- app/routes/background_processes.js
+++ app/routes/background_processes.js
@@ -12,6 +12,13 @@ router.get('/api/v1.0/background-process/org/:org_id', async (req, res) => {
   await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
 });
 
+router.get('/api/v1.0/background-process/:id', async (req, res) => {
+  const{id}  = req.params;    
+  const conn = await connection(dbConfig).catch(e => {return e;});     
+  const sql = `CALL sp_background_process_record_view(${id})`;         
+  await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
+});
+
 
 
 module.exports = router
