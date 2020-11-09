371733cbc132808316716edd420794f73e295526
Gideon kosgei <mr.gkosgei@gmail.com>
2020-11-09 16:07:48 +0300

    administrative units endpoint refined

--- app/routes/country.js
+++ app/routes/country.js
@@ -20,42 +20,14 @@ router.get('/api/v1.0/countries', async (req, res) => {
      res.json({ payload });
    });
 
-   //get regions or counties
-   router.get('/api/v1.0/admin-units/country/:id', async (req, res) => {
+   //get admin units
+   router.get('/api/v1.0/admin-units/:unit/:option', async (req, res) => {
     const conn = await connection(dbConfig).catch(e => {return e;}); 
-    const {id} = req.params;    
-    const sql = `select distinct county_id,county_name from v_administrative_units where country_id = ${id}`;    
+    const {unit,option} = req.params;    
+    const sql = `CALL sp_administrative_units_view(${unit},${option})`;    
     const payload = await query(conn, sql).catch(e=>{return e;});   
     res.json({ payload });
   });
    
-  //get sub-county  or district
-  router.get('/api/v1.0/admin-units/country/:country_id/county/:county_id', async (req, res) => {
-    const conn = await connection(dbConfig).catch(e => {return e;}); 
-    const {country_id,county_id} = req.params;       
-    const sql = `select  distinct sub_county_id,sub_county_name from v_administrative_units where country_id= ${country_id} and county_id = ${county_id}`;    
-    const payload = await query(conn, sql).catch(e=>{return e;});   
-    res.json({ payload });
-  });
-
-
-  //get ward or woreda 
-  router.get('/api/v1.0/admin-units/country/:country_id/county/:county_id/sub-county/:sub_county_id', async (req, res) => {
-    const conn = await connection(dbConfig).catch(e => {return e;}); 
-    const {country_id,county_id,sub_county_id} = req.params;       
-    const sql = `select  distinct ward_id, ward_name from v_administrative_units where country_id= ${country_id} and county_id = ${county_id} and sub_county_id = ${sub_county_id}`;    
-    const payload = await query(conn, sql).catch(e=>{return e;});   
-    res.json({ payload });
-  });
-
-  //get village or kebele
-  router.get('/api/v1.0/admin-units/country/:country_id/county/:county_id/sub-county/:sub_county_id/ward/:ward_id', async (req, res) => {
-    const conn = await connection(dbConfig).catch(e => {return e;}); 
-    const{country_id,county_id,sub_county_id,ward_id }= req.params;     
-    const sql = `select  distinct village_id, village_name from v_administrative_units where country_id= ${country_id} and county_id = ${county_id} and sub_county_id = ${sub_county_id} and ward_id = ${ward_id} `;    
-    const payload = await query(conn, sql).catch(e=>{return e;});   
-    res.json({ payload });
-  });
-
 
  module.exports = router
