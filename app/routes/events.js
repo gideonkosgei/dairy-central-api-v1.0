173182422cbcca798d1aa5d187a16101180ce392
Gideon kosgei <mr.gkosgei@gmail.com>
2021-02-09 15:36:16 +0300

    new endpoint  - view vaccination records

--- app/routes/events.js
+++ app/routes/events.js
@@ -526,7 +526,7 @@ router.put('/api/v1.0/events/vaccination/:id', async (req, res) => {
 router.get('/api/v1.0/events/vaccination/:parameter/:option', async (req, res) => {   
     const {parameter,option} = req.params;   
     const conn = await connection(dbConfig).catch(e => {return e;});     
-    const sql = `CALL sp_event_hoof_treatment_view(${parameter},${option})`;   
+    const sql = `CALL sp_event_vaccination_view(${parameter},${option})`;   
     await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
   });
 
