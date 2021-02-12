004595adeae48646311e3c3194094ed25a9cd0da
Gideon kosgei <mr.gkosgei@gmail.com>
2021-02-12 11:57:50 +0300

    modified stat route - Added option parameter on stats/health-management-trends endpoint

--- app/routes/stats.js
+++ app/routes/stats.js
@@ -59,10 +59,10 @@ router.get('/api/v1.0/stats/weight-growth-curve/:option/:animal_id', async (req,
   await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
 });
 
-router.get('/api/v1.0/stats/health-management-trends/:animal_id', async (req, res) => {   
-  const {animal_id} = req.params;   
+router.get('/api/v1.0/stats/health-management-trends/:option/:id', async (req, res) => {   
+  const {id,option} = req.params;   
   const conn = await connection(dbConfig).catch(e => {return e;});     
-  const sql = `CALL sp_analytics_health_management_trends(${animal_id})`;     
+  const sql = `CALL sp_analytics_health_management_trends(${option},${id})`;     
   await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
 });
 module.exports = router
