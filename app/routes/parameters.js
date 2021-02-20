c95b41ee5fe73c2409e061cc89827a1e0684694c
Gideon kosgei <mr.gkosgei@gmail.com>
2021-02-20 17:03:14 +0300

    new endpoint - get lactation number

--- app/routes/parameters.js
+++ app/routes/parameters.js
@@ -66,5 +66,13 @@ router.put('/api/v1.0/parameters/local-settings/param/:param_id', async (req, re
   const sql = `CALL sp_update_parameter_local_settings(${param_id},${JSON.stringify(key)},${JSON.stringify(value)},${is_active} ,${JSON.stringify(description)},${farm_id},${updated_by})`; 
   await query(conn, sql).then(e => {res.status(200).json({status:200, message:"success"})}).catch(e=>{res.status(400).json({status:400, message:e })});      
 });
+
+// Get lactation Number
+router.get('/api/v1.0/parameters/lactation-number/:option/:animal_id', async (req, res) => {
+  const {option,animal_id} = req.params;    
+  const conn = await connection(dbConfig).catch(e => {return e;});     
+  const sql = `CALL sp_get_lactation_number(${option},${animal_id})`;         
+  await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
+});
  
 module.exports = router
