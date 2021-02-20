a9220b08f1ab3ae472090391c5ead98f43dc3b1c
Gideon kosgei <mr.gkosgei@gmail.com>
2021-02-20 23:31:21 +0300

    reworks - add calving. Enable Twin calving

--- app/routes/events.js
+++ app/routes/events.js
@@ -201,12 +201,105 @@ router.get('/api/v1.0/events/calving/:id', async (req, res) => {
 });
 
 //create Calving event
+/*
 router.post('/api/v1.0/events/calving', async (req, res) => {      
     const conn = await connection(dbConfig).catch(e => {return e;});       
     const {animal_id ,calving_date,birth_type ,body_condition_score ,calf_color ,calf_deformities ,other_calf_deformities , heart_girth,calf_name ,calf_sex ,calf_weight, ease_of_calving_other ,calving_method ,calving_type_other ,calving_type ,ease_of_calving ,calving_status ,use_of_calf ,use_of_calf_other ,calf_tag_id , lactation_number ,field_agent_id ,created_by } = req.body;                       
     const sql = `CALL sp_create_event_calving(${animal_id} ,${JSON.stringify(calving_date)},${birth_type} ,${body_condition_score} ,${calf_color} ,${calf_deformities} ,${JSON.stringify(other_calf_deformities)} , ${heart_girth},${JSON.stringify(calf_name)} ,${calf_sex} ,${calf_weight},${JSON.stringify(ease_of_calving_other)} ,${calving_method} ,${JSON.stringify(calving_type_other)} ,${calving_type} ,${ease_of_calving} ,${calving_status} ,${use_of_calf} ,${JSON.stringify(use_of_calf_other)} ,${JSON.stringify(calf_tag_id)},${lactation_number} ,${JSON.stringify(field_agent_id)} ,${created_by} )`; 
     await query(conn, sql).then(e => {res.status(200).json({status:200, message:"success"})}).catch(e=>{res.status(400).json({status:400, message:e })});      
+});*/
+
+//create Calving event
+router.post('/api/v1.0/events/calving', async (req, res) => {      
+    const conn = await connection(dbConfig).catch(e => {return e;}); 
+    const option = 0;      
+    const {        
+        animal_id,
+        calving_date,
+        birth_type,
+        calving_method,
+        lactation_number,
+        field_agent_id,
+        created_by,        
+        body_condition_score,
+        calf_color,
+        calf_deformities,
+        other_calf_deformities,
+        heart_girth,
+        calf_name,
+        calf_sex,
+        calf_weight,
+        ease_of_calving_other,      
+        calving_type_other,
+        calving_type,
+        ease_of_calving,
+        calving_status,
+        use_of_calf,
+        use_of_calf_other,
+        calf_tag_id,
+        body_condition_score2,
+        calf_color2,
+        calf_deformities2,
+        other_calf_deformities2,
+        heart_girth2,
+        calf_name2,
+        calf_sex2,
+        calf_weight2,
+        ease_of_calving_other2,      
+        calving_type_other2,
+        calving_type2,
+        ease_of_calving2,
+        calving_status2,
+        use_of_calf2,
+        use_of_calf_other2,
+        calf_tag_id2        
+     } = req.body; 
+
+    const sql = `CALL sp_CreateOrUpdateCalvingEventRecord(         
+        ${option},
+        ${animal_id},
+        ${JSON.stringify(calving_date)},
+        ${birth_type},
+        ${lactation_number},
+        ${calving_method},
+        ${JSON.stringify(field_agent_id)},
+        ${created_by},
+        ${body_condition_score},
+        ${calf_color},
+        ${calf_deformities},
+        ${JSON.stringify(other_calf_deformities)}, 
+        ${heart_girth},
+        ${JSON.stringify(calf_name)},
+        ${calf_sex},
+        ${calf_weight},
+        ${JSON.stringify(ease_of_calving_other)},      
+        ${JSON.stringify(calving_type_other)},
+        ${calving_type},
+        ${ease_of_calving},
+        ${calving_status} ,
+        ${use_of_calf},
+        ${JSON.stringify(use_of_calf_other)},
+        ${JSON.stringify(calf_tag_id)},
+        ${body_condition_score2},
+        ${calf_color2},
+        ${calf_deformities2},
+        ${JSON.stringify(other_calf_deformities2)}, 
+        ${heart_girth2},
+        ${JSON.stringify(calf_name2)},
+        ${calf_sex2},
+        ${calf_weight2},
+        ${JSON.stringify(ease_of_calving_other2)},      
+        ${JSON.stringify(calving_type_other2)},
+        ${calving_type2},
+        ${ease_of_calving2},
+        ${calving_status2},
+        ${use_of_calf2},
+        ${JSON.stringify(use_of_calf_other2)},
+        ${JSON.stringify(calf_tag_id2)} 
+    )`;    
+    await query(conn, sql).then(e => {res.status(200).json({status:200, message:"success"})}).catch(e=>{res.status(400).json({status:400, message:e })});      
 });
+
 
 //update Calving event record
 router.put('/api/v1.0/events/calving/:event_id', async (req, res) => {  
@@ -216,8 +309,6 @@ router.put('/api/v1.0/events/calving/:event_id', async (req, res) => {
     const sql = `CALL sp_update_event_calving(${event_id} ,${JSON.stringify(calving_date)},${birth_type} ,${body_condition_score} ,${calf_color} ,${calf_deformities} ,${JSON.stringify(other_calf_deformities)} , ${heart_girth},${JSON.stringify(calf_name)} ,${calf_sex} ,${calf_weight},${JSON.stringify(ease_of_calving_other)} ,${calving_method} ,${JSON.stringify(calving_type_other)} ,${calving_type} ,${ease_of_calving} ,${calving_status} ,${use_of_calf} ,${JSON.stringify(use_of_calf_other)} ,${JSON.stringify(calf_tag_id)},${lactation_number} ,${JSON.stringify(field_agent_id)} ,${updated_by} )`; 
     await query(conn, sql).then(e => {res.status(200).json({status:200, message:"success"})}).catch(e=>{res.status(400).json({status:400, message:e })});      
 });
-
-   
 
 //Milking events
 //view miking records
@@ -255,11 +346,9 @@ router.get('/api/v1.0/events/milking/:id', async (req, res) => {
 router.get('/api/v1.0/events/milking/parameters/:animal_id/:milk_date', async (req, res) => {      
     const conn = await connection(dbConfig).catch(e => {return e;});     
     const {animal_id,milk_date} = req.params;
-    const sql = `CALL sp_set_milking_parameters(${animal_id},${JSON.stringify(milk_date)})`;
+    const sql = `CALL sp_set_milking_parameters(${animal_id},${JSON.stringify(milk_date)})`; 
     await query(conn, sql).then(response => {res.status(200).json({payload:response})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
 });
-
-
 
 //health events
 //view health records
