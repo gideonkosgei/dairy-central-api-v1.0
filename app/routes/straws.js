38ed7c29a233c418c21ed3f2ffc7b4bc98141600
Gideon kosgei <mr.gkosgei@gmail.com>
2021-04-20 14:32:47 +0300

    loader - create & update ai straw

--- app/routes/straws.js
+++ app/routes/straws.js
@@ -13,18 +13,36 @@ router.get('/api/v1.0/straws/:id/:option/:active', async (req, res) => {
 
 
 router.post('/api/v1.0/straws', async (req, res) => {        
+  try{
   const conn = await connection(dbConfig).catch(e => {return e;}); 
   const {straw_id,barcode,bull_tag_id,bull_name,breed,breed_composition,semen_source,origin_country,farm_name,batch_number,ejaculation_number,production_date,specification,additional_info,org_id,created_by} = req.body; 
   const sql = `CALL sp_ai_straw_create(${JSON.stringify(straw_id)}, ${JSON.stringify(barcode)}, ${JSON.stringify(bull_tag_id)}, ${JSON.stringify(bull_name)} ,${breed}, ${breed_composition}, ${semen_source},${origin_country}, ${JSON.stringify(farm_name)},${JSON.stringify(batch_number)} ,${JSON.stringify(ejaculation_number)} ,${JSON.stringify(production_date)}, ${JSON.stringify(specification)} ,${JSON.stringify(additional_info)} ,${org_id} ,${created_by} )`;   
-  await query(conn, sql).then(e => {res.status(200).json({status:200, message:"success"})}).catch(e=>{res.status(400).json({status:400, message:e })});      
+ 
+  await query(conn, sql).then(
+    response => {            
+    res.status(200).json({status:response[0][0].status,message:response[0][0].message}) 
+  })
+  .catch(e => {res.status(400).json({status:400, message:e })});
+} catch(error) {
+      res.send({status:0,message:`system error! ${error.message}`});
+  }   
 });
 
-router.put('/api/v1.0/straws/:id', async (req, res) => {        
+router.put('/api/v1.0/straws/:id', async (req, res) => { 
+  try{       
   const conn = await connection(dbConfig).catch(e => {return e;}); 
   const{id}  = req.params;
   const {straw_id,barcode,bull_tag_id,bull_name,breed,breed_composition,semen_source,farm_name,batch_number,ejaculation_number,production_date,specification,additional_info,updated_by,is_active,origin_country} = req.body; 
   const sql = `CALL sp_ai_straw_update(${id},${JSON.stringify(straw_id)}, ${JSON.stringify(barcode)}, ${JSON.stringify(bull_tag_id)}, ${JSON.stringify(bull_name)} ,${breed}, ${breed_composition}, ${semen_source},${origin_country}, ${JSON.stringify(farm_name)},${JSON.stringify(batch_number)} ,${JSON.stringify(ejaculation_number)} ,${JSON.stringify(production_date)}, ${JSON.stringify(specification)} ,${JSON.stringify(additional_info)},${updated_by},${is_active} )`;   
-  await query(conn, sql).then(e => {res.status(200).json({status:200, message:"success"})}).catch(e=>{res.status(400).json({status:400, message:e })});      
+  
+  await query(conn, sql).then(
+    response => {            
+    res.status(200).json({status:response[0][0].status,message:response[0][0].message}) 
+  })
+  .catch(e => {res.status(400).json({status:400, message:e })});
+} catch(error) {
+      res.send({status:0,message:`system error! ${error.message}`});
+  } 
 });
 
 
