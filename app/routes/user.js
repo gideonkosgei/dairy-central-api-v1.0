83a84b6dcb00ff0d333e3ad67f98e22fd180dbbe
Gideon kosgei <mr.gkosgei@gmail.com>
2021-05-05 20:33:07 +0300

    org- upload logo

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,13 +1,38 @@
 const express = require("express");
 var router = express.Router();
 const path = require("path");
-const multer = require("multer");
+
+const multer = require('multer');
+
 const bcrypt = require("bcrypt");
 const nodemailer = require("nodemailer");
 
 const dbConfig = require('../config/dbConfig.js');
 const connection = require('../helpers/connection');
 const query = require('../helpers/query');
+
+/** configs for image uploads */
+/*const storage = multer.diskStorage({
+  destination: "./public/uploads/",
+  filename: function(req, file, cb){
+     cb(null,"IMAGE-" + Date.now() + path.extname(file.originalname));
+  }
+}); */   
+
+const storage = multer.diskStorage({
+  destination: (req,file,cb)=> {
+    cb(null,"./");
+  },
+  filename: function(req,file,cb){
+    const ext = file.mimetype.split("/")[1];
+    cb(null,`public/uploads/${file.originalname}-${Date.now()}.${ext}`);
+  }
+}); 
+
+const upload = multer({
+  storage: storage 
+});
+
 
 /**
  * Generate randon string as password
@@ -144,39 +169,31 @@ router.get('/api/v1.0/user/:id', async (req, res) => {
 
 
   /** Upload profile Pic */ 
-  router.post('/api/v1.0/org/upload/profile-logo', async (req, res) => { 
-    
-      const storage = multer.diskStorage({
-        destination: "./public/uploads/",
-        filename: function(req, file, cb){
-           cb(null,"IMAGE-" + Date.now() + path.extname(file.originalname));
-        }
-      });      
-      const upload = multer({
-        storage: storage,
-        limits:{fileSize: 1000000},
-      }).single("myImage");
-
-      console.log(req.files);
-
-      upload(req,res,function(err) { 
-        if(err) { 
-            res.send(err) 
-        } 
-        else { 
-            // SUCCESS, image successfully uploaded 
-            res.send("Success, Image uploaded!") 
-        } 
-    }) ;
+  router.post('/api/v1.0/org/upload/profile-logo/:org/:user/:type', upload.single('image'), async (req, res) => {  
+    try{  
+      const {org,user,type} = req.params;
+      const conn = await connection(dbConfig).catch(e => {return e;}); 
+      const account = (parseInt(type) === 0) ? org : user;
      
- 
-    //const {org,user_id,image} = req.body;   
-    //console.log(req);  
-    //const conn = await connection(dbConfig).catch(e => {return e;}); 
-    //const sql ='' ; 
-    //const sql = `CALL sp_view_org_details(${id})`;
-    //await query(conn, sql).then(response => {res.status(200).json({payload:response})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
-  });
+      
+      if (!req.file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) { 
+        res.send({status:0,message:'Only image files (jpg, jpeg, png) are allowed!'})
+      } else {             
+     
+      const sql = `CALL sp_createOrUpdateUserAccountAvatar(${type},${account},${JSON.stringify(req.file.originalname)},${JSON.stringify(req.file.filename)},${JSON.stringify(req.file.path)},${req.file.size},${user})`;  
+    
+      await query(conn, sql).then(
+          response => {            
+          res.status(200).json({status:response[0][0].status,message:response[0][0].message}) 
+        })
+        .catch(e => {res.status(400).json({status:400, message:e })});
+      }
+
+      } catch(error) {
+        res.send({status:0,message:`system error! ${error.message}`});
+    }  
+     
+   });
 
   router.put('/api/v1.0/user/account/change-password/self-service', async (req, res) => { 
     try{
@@ -231,7 +248,7 @@ router.get('/api/v1.0/user/:id', async (req, res) => {
         .then(response => {   
           email_settings = {
             email_host:response[0][0].email_host,    
-            email_port:response[0][0].email_port,
+            email_port:parseInt(response[0][0].email_port),
             email_username:response[0][0].email_username,
             email_password:response[0][0].email_password,
             email_security:response[0][0].email_security, 
@@ -245,8 +262,7 @@ router.get('/api/v1.0/user/:id', async (req, res) => {
         });  
         
         await query(conn, sql3)
-        .then(response => {          
-
+        .then(response => {  
           email_template = {
             id:response[0][0].id,
             subject:response[0][0].subject,
@@ -258,8 +274,28 @@ router.get('/api/v1.0/user/:id', async (req, res) => {
           res.status(400).json({
             status:400, message:e 
           })
-        });      
+        }); 
+
+        /*
         
+        const transporter = nodemailer.createTransport({
+          host: email_settings.email_host,
+          port: email_settings.email_host,
+          secure: false,
+          auth: {
+            user: "g.kipkosgei@cgiar.org", 
+            pass: "Trump2024", 
+          },
+        });
+       
+        const mailOptions = {
+          from: "g.kipkosgei@cgiar.org",
+          to: 'mr.gkosgei@gmail.com',
+          subject: email_template.subject,         
+          html: `<b>Hey ${email}! </b><br/> Your new password is ${plain_text_password} <br/>`
+        };  
+        */
+       
         const transporter = nodemailer.createTransport({
           host: 'smtp.office365.com',
           port: 587,
@@ -267,13 +303,13 @@ router.get('/api/v1.0/user/:id', async (req, res) => {
           auth: {
             user: 'g.kipkosgei@cgiar.org', 
             pass: 'Trump2024', 
-          },
+          }
         });
        
         const mailOptions = {
           from: 'g.kipkosgei@cgiar.org',
           to: 'mr.gkosgei@gmail.com',
-          subject: email_template.subject,         
+          subject: "password change",         
           html: `<b>Hey ${email}! </b><br/> Your new password is ${plain_text_password} <br/>`
         };
 
