2715dde23aced602e02f5f221156e4802b1215a8
Gideon kosgei <mr.gkosgei@gmail.com>
2021-05-31 18:43:18 +0300

    org access route changes

--- app/routes/orgs.js
+++ app/routes/orgs.js
@@ -13,7 +13,7 @@ router.get('/api/v1.0/orgs', async (req, res) => {
 router.get('/api/v1.0/orgs/:option/:id', async (req, res) => {  
   const {id,option} = req.params;   
   const conn = await connection(dbConfig).catch(e => {return e;});     
-  const sql = `CALL sp_ViewOrgOrOrgs(${option},${id})`;     
+  const sql = `CALL sp_ViewOrgOrOrgs(${option},${id})`;  
   await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
 });
 
@@ -68,15 +68,10 @@ router.put('/api/v1.0/org', async (req, res) => {
     res.send({status:0,message:`system error! ${error.message}`});
   }   
 });
-
-
-
-
-
-router.get('/api/v1.0/orgs/access/:id', async (req, res) => {   
+router.get('/api/v1.0/org/access/:id', async (req, res) => {   
   const user_id = req.params.id;   
   const conn = await connection(dbConfig).catch(e => {return e;});     
-  const sql = `CALL sp_org_access_view(${user_id})`;        
+  const sql = `CALL sp_org_access_view(${user_id})`;       
   await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
 });
 
