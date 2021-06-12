8a8ce090500813ddf32d38b200dce3e54d8df53f
Gideon kosgei <mr.gkosgei@gmail.com>
2021-06-13 02:42:01 +0300

    country route - added catch

--- app/routes/country.js
+++ app/routes/country.js
@@ -8,16 +8,16 @@ const query = require('../helpers/query');
 router.get('/api/v1.0/countries', async (req, res) => {
     const conn = await connection(dbConfig).catch(e => {return e;});     
     const sql = "select id,code,name,country,unit1_name,unit2_name,unit3_name,unit4_name from core_country where is_active=1";    
-    const payload = await query(conn, sql).catch(e=>{return e;});   
-    res.json({ payload });
+    await query(conn, sql).then(response => {res.status(200).json({payload:response})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
+       
   });
 
   router.get('/api/v1.0/countries/:id', async (req, res) => {
      const conn = await connection(dbConfig).catch(e => {return e;}); 
      const {id} = req.params;
      const sql = `select id,code,name,country,unit1_name,unit2_name,unit3_name,unit4_name from core_country where  id = ${id}`;    
-     const payload = await query(conn, sql).catch(e=>{return e;});   
-     res.json({ payload });
+     await query(conn, sql).then(response => {res.status(200).json({payload:response})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
+    
    });
 
    //get admin units
@@ -25,8 +25,8 @@ router.get('/api/v1.0/countries', async (req, res) => {
     const conn = await connection(dbConfig).catch(e => {return e;}); 
     const {unit,option} = req.params;    
     const sql = `CALL sp_administrative_units_view(${unit},${option})`;    
-    const payload = await query(conn, sql).catch(e=>{return e;});   
-    res.json({ payload });
+    await query(conn, sql).then(response => {res.status(200).json({payload:response})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
+    
   });
 
  module.exports = router
