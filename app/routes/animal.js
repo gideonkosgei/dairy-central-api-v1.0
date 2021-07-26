6228b793935a0d4906b8afc7b18e28f68c11e14e
Gideon kosgei <mr.gkosgei@gmail.com>
2021-07-26 15:01:59 +0300

    animal list changes

--- app/routes/animal.js
+++ app/routes/animal.js
@@ -22,7 +22,7 @@ const query = require('../helpers/query');
  router.get('/api/v1.0/animal/type/:id/:type/:option', async (req, res) => {      
   const conn = await connection(dbConfig).catch(e => {return e;});     
   const {id,type,option} = req.params;
-  const sql = `CALL sp_view_animals_by_animal_type(${option},${id},${type})`;         
+  const sql = `CALL sp_view_animals_by_animal_type(${option},${id},${type})`; 
   await query(conn, sql).then(response => {res.status(200).json({payload:response})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
 });
 
