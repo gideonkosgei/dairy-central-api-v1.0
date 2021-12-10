bb1f8ca635e168c5d38df179030dc7ffae2f25c4
Gideon kosgei <mr.gkosgei@gmail.com>
2021-12-10 15:37:24 +0300

    events summary endpoint

--- app/routes/stats.js
+++ app/routes/stats.js
@@ -4,87 +4,95 @@ const dbConfig = require('../config/dbConfig.js');
 const connection = require('../helpers/connection');
 const query = require('../helpers/query');
 
-router.get('/api/v1.0/stats/top-cows/:option/:org/:year', async (req, res) => {   
-  const {org,year,option} = req.params;   
-  const conn = await connection(dbConfig).catch(e => {return e;});     
-  const sql = `CALL sp_stats_top_cows(${option},${org},${year})`;      
-  await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
+router.get('/api/v1.0/stats/top-cows/:option/:org/:year', async (req, res) => {
+  const { org, year, option } = req.params;
+  const conn = await connection(dbConfig).catch(e => { return e; });
+  const sql = `CALL sp_stats_top_cows(${option},${org},${year})`;
+  await query(conn, sql).then(response => { res.status(200).json({ payload: response[0] }) }).catch(e => { res.status(400).json({ status: 400, message: e }) });
 });
 
-router.get('/api/v1.0/stats/milk-performance-comparator/:org', async (req, res) => {   
-  const {org} = req.params;   
-  const conn = await connection(dbConfig).catch(e => {return e;});     
-  const sql = `CALL sp_stats_milk_pre_cur_year_comparison(${org})`;        
-  await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
+router.get('/api/v1.0/stats/milk-performance-comparator/:org', async (req, res) => {
+  const { org } = req.params;
+  const conn = await connection(dbConfig).catch(e => { return e; });
+  const sql = `CALL sp_stats_milk_pre_cur_year_comparison(${org})`;
+  await query(conn, sql).then(response => { res.status(200).json({ payload: response[0] }) }).catch(e => { res.status(400).json({ status: 400, message: e }) });
 });
 
-router.get('/api/v1.0/stats/breed-distribution/:org/:level/:herd', async (req, res) => {   
-  const {org,level,herd} = req.params;   
-  const conn = await connection(dbConfig).catch(e => {return e;});     
-  const sql = `CALL sp_stats_breed_distribution(${org},${level},${herd})`;        
-  await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
+router.get('/api/v1.0/stats/breed-distribution/:org/:level/:herd', async (req, res) => {
+  const { org, level, herd } = req.params;
+  const conn = await connection(dbConfig).catch(e => { return e; });
+  const sql = `CALL sp_stats_breed_distribution(${org},${level},${herd})`;
+  await query(conn, sql).then(response => { res.status(200).json({ payload: response[0] }) }).catch(e => { res.status(400).json({ status: 400, message: e }) });
 });
 
-router.get('/api/v1.0/stats/dashboard-overview/:org', async (req, res) => {   
-  const {org} = req.params;   
-  const conn = await connection(dbConfig).catch(e => {return e;});     
-  const sql = `CALL sp_stats_dashboard_overview(${org})`;        
-  await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
+router.get('/api/v1.0/stats/dashboard-overview/:org', async (req, res) => {
+  const { org } = req.params;
+  const conn = await connection(dbConfig).catch(e => { return e; });
+  const sql = `CALL sp_stats_dashboard_overview(${org})`;
+  await query(conn, sql).then(response => { res.status(200).json({ payload: response[0] }) }).catch(e => { res.status(400).json({ status: 400, message: e }) });
 });
 
-router.get('/api/v1.0/stats/lactation-table/:animal_id', async (req, res) => {   
-  const {animal_id} = req.params;   
-  const conn = await connection(dbConfig).catch(e => {return e;});     
-  const sql = `CALL sp_analytics_milking_trends(${animal_id})`;     
-  await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
+router.get('/api/v1.0/stats/lactation-table/:animal_id', async (req, res) => {
+  const { animal_id } = req.params;
+  const conn = await connection(dbConfig).catch(e => { return e; });
+  const sql = `CALL sp_analytics_milking_trends(${animal_id})`;
+  await query(conn, sql).then(response => { res.status(200).json({ payload: response[0] }) }).catch(e => { res.status(400).json({ status: 400, message: e }) });
 });
-router.get('/api/v1.0/stats/lactation-curve/:animal_id', async (req, res) => {   
-  const {animal_id} = req.params;   
-  const conn = await connection(dbConfig).catch(e => {return e;});     
-  const sql = `CALL sp_analytics_lactation_curve(${animal_id})`;     
-  await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
-});
-
-router.get('/api/v1.0/stats/herd-milking-summary/:report_type/:org_id', async (req, res) => {   
-  const {report_type,org_id} = req.params;   
-  const conn = await connection(dbConfig).catch(e => {return e;});     
-  const sql = `CALL sp_analytics_herd_milking_summary(${report_type},${org_id})`;     
-  await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
+router.get('/api/v1.0/stats/lactation-curve/:animal_id', async (req, res) => {
+  const { animal_id } = req.params;
+  const conn = await connection(dbConfig).catch(e => { return e; });
+  const sql = `CALL sp_analytics_lactation_curve(${animal_id})`;
+  await query(conn, sql).then(response => { res.status(200).json({ payload: response[0] }) }).catch(e => { res.status(400).json({ status: 400, message: e }) });
 });
 
-router.get('/api/v1.0/stats/weight-growth-curve/:option/:type/:animal_id', async (req, res) => {   
-  const {animal_id,type,option} = req.params;   
-  const conn = await connection(dbConfig).catch(e => {return e;});     
-  const sql = `CALL sp_analytics_weight_growth_curve(${option},${type},${animal_id})`;     
-  await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
+router.get('/api/v1.0/stats/herd-milking-summary/:report_type/:org_id', async (req, res) => {
+  const { report_type, org_id } = req.params;
+  const conn = await connection(dbConfig).catch(e => { return e; });
+  const sql = `CALL sp_analytics_herd_milking_summary(${report_type},${org_id})`;
+  await query(conn, sql).then(response => { res.status(200).json({ payload: response[0] }) }).catch(e => { res.status(400).json({ status: 400, message: e }) });
 });
 
-router.get('/api/v1.0/stats/health-management-trends/:option/:id/:date_start/:date_end', async (req, res) => {   
-  const {id,option,date_start,date_end} = req.params;   
-  const conn = await connection(dbConfig).catch(e => {return e;});     
-  const sql = `CALL sp_analytics_health_management_trends(${option},${id},${JSON.stringify(date_start)},${JSON.stringify(date_end)})`;  
-  await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
+router.get('/api/v1.0/stats/weight-growth-curve/:option/:type/:animal_id', async (req, res) => {
+  const { animal_id, type, option } = req.params;
+  const conn = await connection(dbConfig).catch(e => { return e; });
+  const sql = `CALL sp_analytics_weight_growth_curve(${option},${type},${animal_id})`;
+  await query(conn, sql).then(response => { res.status(200).json({ payload: response[0] }) }).catch(e => { res.status(400).json({ status: 400, message: e }) });
 });
 
-router.get('/api/v1.0/stats/due-dates/:org/:option', async (req, res) => {   
-  const {org,option} = req.params;   
-  const conn = await connection(dbConfig).catch(e => {return e;});     
-  const sql = `CALL sp_animals_due_dates(${org},${option})`; 
-  await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
+router.get('/api/v1.0/stats/health-management-trends/:option/:id/:date_start/:date_end', async (req, res) => {
+  const { id, option, date_start, date_end } = req.params;
+  const conn = await connection(dbConfig).catch(e => { return e; });
+  const sql = `CALL sp_analytics_health_management_trends(${option},${id},${JSON.stringify(date_start)},${JSON.stringify(date_end)})`;
+  await query(conn, sql).then(response => { res.status(200).json({ payload: response[0] }) }).catch(e => { res.status(400).json({ status: 400, message: e }) });
 });
 
-router.get('/api/v1.0/stats/pd-action-list/:org/:option', async (req, res) => {   
-  const {org,option} = req.params;   
-  const conn = await connection(dbConfig).catch(e => {return e;});     
-  const sql = `CALL sp_animals_pd_action_list(${org},${option})`;     
-  await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
+router.get('/api/v1.0/stats/due-dates/:org/:option', async (req, res) => {
+  const { org, option } = req.params;
+  const conn = await connection(dbConfig).catch(e => { return e; });
+  const sql = `CALL sp_animals_due_dates(${org},${option})`;
+  await query(conn, sql).then(response => { res.status(200).json({ payload: response[0] }) }).catch(e => { res.status(400).json({ status: 400, message: e }) });
 });
 
-router.get('/api/v1.0/stats/service-action-list/:org/:option', async (req, res) => {   
-  const {org,option} = req.params;   
-  const conn = await connection(dbConfig).catch(e => {return e;});     
+router.get('/api/v1.0/stats/pd-action-list/:org/:option', async (req, res) => {
+  const { org, option } = req.params;
+  const conn = await connection(dbConfig).catch(e => { return e; });
+  const sql = `CALL sp_animals_pd_action_list(${org},${option})`;
+  await query(conn, sql).then(response => { res.status(200).json({ payload: response[0] }) }).catch(e => { res.status(400).json({ status: 400, message: e }) });
+});
+
+router.get('/api/v1.0/stats/service-action-list/:org/:option', async (req, res) => {
+  const { org, option } = req.params;
+  const conn = await connection(dbConfig).catch(e => { return e; });
   const sql = `CALL sp_analytics_service_action_list(${org},${option})`;
-  await query(conn, sql).then(response => {res.status(200).json({payload:response[0]})}).catch(e=>{res.status(400).json({status:400, message:e })}); 
+  await query(conn, sql).then(response => { res.status(200).json({ payload: response[0] }) }).catch(e => { res.status(400).json({ status: 400, message: e }) });
+});
+
+
+router.get('/api/v1.0/stats/events-summary/:org/:level/:herd', async (req, res) => {
+  const { org, level, herd } = req.params;
+  const conn = await connection(dbConfig).catch(e => { return e; });
+  const sql = `CALL sp_stats_events_summary(${org},${level},${herd})`;
+  await query(conn, sql).then(response => { res.status(200).json({ payload: response[0] }) }).catch(e => { res.status(400).json({ status: 400, message: e }) });
 });
 
 
